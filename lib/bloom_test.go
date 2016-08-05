98f293d075602eec4a222035e57cd3d7093063ea
Philip Schlump <pschlump@gmail.com>
2016-08-05 13:30:32 -0600

    1st test on hasing passed

--- lib/bloom_test.go
+++ lib/bloom_test.go
@@ -11,7 +11,14 @@ func Test_SuperFashHash(t *testing.T) {
 		Str         string
 		BloomFilter uint32
 	}{
-		{Str: "75872", BloomFilter: 2488709022}, //
+		{Str: "", BloomFilter: 0},                 // 0
+		{Str: "7", BloomFilter: 1172465224},       // 1
+		{Str: "75", BloomFilter: 2471797843},      // 2
+		{Str: "758", BloomFilter: 776002016},      // 3
+		{Str: "7587", BloomFilter: 36914527},      // 4
+		{Str: "75872", BloomFilter: 2488709022},   // 5
+		{Str: "75872a", BloomFilter: 4233351963},  // 6
+		{Str: "7b5872a", BloomFilter: 2661868145}, // 7
 	}
 
 	for ii, test := range tests {
